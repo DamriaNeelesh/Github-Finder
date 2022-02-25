7e6b2da80941cf79428ab779021f5a87f4849661
Hemant Yadav <85957105+Hemant6271@users.noreply.github.com>
2022-02-25 11:04:58 +0530

    Update About.js

--- src/Componemts/About.js
+++ src/Componemts/About.js
@@ -3,7 +3,6 @@ const About= ()=> {
     return (
         <div className="my-5">
             <p>Github Finder is an React App  that allows  to find the github users and watch their profile </p>
-            <p> <a href="https://myportfoliohirdeshgupta.herokuapp.com/index.html#"> Developed by Hirdesh Gupta </a></p>
         </div>
     )
 }
